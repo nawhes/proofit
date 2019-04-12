805878b6e5d59432fbab587b4f28e2766a5202d5
nawhes <nawhes@naver.com>
2019-04-13 06:58:10 +0900

    w

--- proofit.sh
+++ proofit.sh
@@ -4,7 +4,7 @@ export MSYS_NO_PATHCONV=1
 
 docker-compose -f docker-compose.yaml down
 
-docker-compose -f docker-compose.yaml up -d server.ca.com admin.ca.com orderer.ca.com zookeeper.ca.com kafka.ca.com admin.app.com app.app.com appdb recruit.app.com recruitdb admin.language.com toeic.language.com toeicdb admin.qualify.com qnet.qualify.com qnetdb admin.univ.com smu.univ.com smudb
+docker-compose -f docker-compose.yaml up -d server.ca.com admin.ca.com zookeeper.ca.com orderer.ca.com kafka.ca.com admin.app.com app.app.com appdb recruit.app.com recruitdb admin.language.com toeic.language.com toeicdb admin.qualify.com qnet.qualify.com qnetdb admin.univ.com smu.univ.com smudb
 
 export FABRIC_START_TIMEOUT=30
 echo ${FABRIC_START_TIMEOUT}
