aad930c8bb9f10caf8a87d432bdd26394d0407a8
nawhes <nawhes@naver.com>
2019-04-17 21:25:29 +0900

    w

--- license/admin.license.com/fabric-ca-client/license/index.js
+++
@@ -1,8 +0,0 @@
-/*
-SPDX-License-Identifier: Apache-2.0
-*/
-
-'use strict';
-
-const careercontract = require('./lib/careercontract.js');
-module.exports.contracts = [careercontract];
--- univ/admin.univ.com/fabric-ca-client/account/index.js
+++
@@ -1,8 +0,0 @@
-/*
-SPDX-License-Identifier: Apache-2.0
-*/
-
-'use strict';
-
-const accountcontract = require('./lib/accountcontract.js');
-module.exports.contracts = [accountcontract];
