ca3a8af3f3ed8cd852bf95a51a6eac5430476396
nawhes <nawhes@naver.com>
2019-04-17 21:30:07 +0900

    w

--- z_instantiatechaincode.sh
+++ z_instantiatechaincode.sh
@@ -1,12 +1,12 @@
 echo "###[admin.app.com] instantiate account chaincode"
-docker exec -e admin.app.com peer chaincode instantiate -n account -v beta0 -C account -c '{"Args":["account:instantiate"]}'
+docker exec -e admin.app.com peer chaincode instantiate -n account -v beta0 -C account -c '{"Args":["account:instantiate"]}' -P "AND ('app.member')"
 
 echo "###[admin.app.com] instantiate univ chaincode"
-docker exec -e admin.app.com peer chaincode instantiate -n univ -v beta0 -C account -c '{"Args":["univ:instantiate"]}'
+docker exec -e admin.app.com peer chaincode instantiate -n univ -v beta0 -C account -c '{"Args":["univ:instantiate"]}' -P "AND ('univ.member')"
 
 echo "###[admin.app.com] instantiate license chaincode"
-docker exec -e admin.app.com peer chaincode instantiate -n license -v beta0 -C account -c '{"Args":["license:instantiate"]}'
+docker exec -e admin.app.com peer chaincode instantiate -n license -v beta0 -C account -c '{"Args":["license:instantiate"]}' -P "AND ('license.member')"
 
 echo "###[admin.app.com] instantiate language chaincode"
-docker exec -e admin.app.com peer chaincode instantiate -n language -v beta0 -C account -c '{"Args":["language:instantiate"]}'
+docker exec -e admin.app.com peer chaincode instantiate -n language -v beta0 -C account -c '{"Args":["language:instantiate"]}' -P "AND ('language.member')"
            
