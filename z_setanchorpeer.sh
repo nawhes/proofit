d3c7a271d1cd147991d62042441690049478a46c
nawhes <nawhes@naver.com>
2019-05-08 22:39:18 +0900

    w

--- z_setanchorpeer.sh
+++ z_setanchorpeer.sh
@@ -2,7 +2,7 @@ echo "###[admin.app.com] set AnchorPeers to account channel"
 docker exec admin.app.com peer channel update -o orderer.ca.com:7050 -c account -f appAnchorPeerAccount.tx #--certfile PATH --cafile PATH --tls
 
 echo "###[admin.app.com] set AnchorPeers to proofit channel"
-docker exec admin.app.com peer channel update -o orderer.ca.com:7050 -c account -f appAnchorPeerProofit.tx #--certfile PATH --cafile PATH --tls
+docker exec admin.app.com peer channel update -o orderer.ca.com:7050 -c proofit -f appAnchorPeerProofit.tx #--certfile PATH --cafile PATH --tls
 
 echo "###[admin.app.com] set AnchorPeers to univ channel"
 docker exec admin.app.com peer channel update -o orderer.ca.com:7050 -c univ -f appAnchorPeerUniv.tx #--certfile PATH --cafile PATH --tls
@@ -15,14 +15,14 @@ echo "###[admin.app.com] set AnchorPeers to language channel"
 
 
 echo "###[admin.recruit.com] set AnchorPeers to recruit channel"
-docker exec admin.recruit.com peer channel update -o orderer.ca.com:7050 -c account -f recruitAnchorPeerRecruit.tx #--certfile PATH --cafile PATH --tls
+docker exec admin.recruit.com peer channel update -o orderer.ca.com:7050 -c proofit -f recruitAnchorPeerProofit.tx #--certfile PATH --cafile PATH --tls
 
 
 echo "###[admin.univ.com] set AnchorPeers to account channel"
 docker exec admin.univ.com peer channel update -o orderer.ca.com:7050 -c account -f univAnchorPeerAccount.tx #--certfile PATH --cafile PATH --tls
 
 echo "###[admin.univ.com] set AnchorPeers to proofit channel"
-docker exec admin.univ.com peer channel update -o orderer.ca.com:7050 -c account -f univAnchorPeerProofit.tx #--certfile PATH --cafile PATH --tls
+docker exec admin.univ.com peer channel update -o orderer.ca.com:7050 -c proofit -f univAnchorPeerProofit.tx #--certfile PATH --cafile PATH --tls
 
 echo "###[admin.univ.com] set AnchorPeers to univ channel"
 docker exec admin.univ.com peer channel update -o orderer.ca.com:7050 -c univ -f univAnchorPeerUniv.tx #--certfile PATH --cafile PATH --tls
@@ -32,7 +32,7 @@ echo "###[admin.license.com] set AnchorPeers to account channel"
 #docker exec admin.license.com peer channel update -o orderer.ca.com:7050 -c account -f licenseAnchorPeerAccount.tx #--certfile PATH --cafile PATH --tls
 
 echo "###[admin.license.com] set AnchorPeers to proofit channel"
-#docker exec admin.license.com peer channel update -o orderer.ca.com:7050 -c account -f licenseAnchorPeerProofit.tx #--certfile PATH --cafile PATH --tls
+#docker exec admin.license.com peer channel update -o orderer.ca.com:7050 -c proofit -f licenseAnchorPeerProofit.tx #--certfile PATH --cafile PATH --tls
 
 echo "###[admin.license.com] set AnchorPeers to license channel"
 #docker exec admin.license.com peer channel update -o orderer.ca.com:7050 -c license -f licenseAnchorPeerLicense.tx #--certfile PATH --cafile PATH --tls
@@ -42,7 +42,7 @@ echo "###[admin.language.com] set AnchorPeers to account channel"
 #docker exec admin.language.com peer channel update -o orderer.ca.com:7050 -c account -f languageAnchorPeerAccount.tx #--certfile PATH --cafile PATH --tls
 
 echo "###[admin.language.com] set AnchorPeers to proofit channel"
-#docker exec admin.language.com peer channel update -o orderer.ca.com:7050 -c account -f languageAnchorPeerProofit.tx #--certfile PATH --cafile PATH --tls
+#docker exec admin.language.com peer channel update -o orderer.ca.com:7050 -c proofit -f languageAnchorPeerProofit.tx #--certfile PATH --cafile PATH --tls
 
 echo "###[admin.language.com] set AnchorPeers to language channel"
 #docker exec admin.language.com peer channel update -o orderer.ca.com:7050 -c language -f languageAnchorPeerLanguage.tx #--certfile PATH --cafile PATH --tls
